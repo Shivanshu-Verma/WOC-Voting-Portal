0d0f9311e37840c91f34e3ec8fa389fa55f1e077
HARSHVARANDANI <hpvarandani@gmail.com>
2025-03-22 23:36:14 +0530

    Updated routes to use new middlewares

--- master_server/src/routes/evm.routes.js
+++ master_server/src/routes/evm.routes.js
@@ -2,6 +2,6 @@ import express from "express";
 import { handleEvmRegistration } from "../controllers/evm.controller.js";
 
 const router = express.Router();
-router.post("/register", handleEvmRegistration); // ec auth
+router.post("/register", authenticateUser, validateEvmRequest, handleEvmRegistration); // ec auth
 
 export default router;
