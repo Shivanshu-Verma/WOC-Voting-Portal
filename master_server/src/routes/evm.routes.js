0dd671ef3d778877f79b14347de5d0680a63b7aa
HARSHVARANDANI <hpvarandani@gmail.com>
2025-03-23 00:18:49 +0530

    Added missing import line for middleware

--- master_server/src/routes/evm.routes.js
+++ master_server/src/routes/evm.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { handleEvmRegistration } from "../controllers/evm.controller.js";
+import { authenticateUser, validateEvmRequest } from "../middlewares/auth.middleware.js"
 
 const router = express.Router();
 router.post("/register", authenticateUser, validateEvmRequest, handleEvmRegistration); // ec auth
