9c2f8051b904588153c1f5394f0d119c1307f12c
HARSHVARANDANI <hpvarandani@gmail.com>
2025-03-27 23:58:00 +0530

    Added routes to handle get requests for all DB data

---
+++ master_server/src/routes/db.routes.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { getAllVoters,
+    getAllCandidates,
+    getAllVolunteers,
+    getAllStaff,
+    getAllEVM,
+    getAllCommitments
+ } from "../controllers/db.controller";
+
+const router = express.Router();
+
+// Route to get all voters
+router.get("/voters", getAllVoters);
+router.get("/candidates", getAllCandidates);
+router.get("/volunteers", getAllVolunteers);
+router.get("/staff", getAllStaff);
+router.get("/evm", getAllEVM);
+router.get("/commitments", getAllCommitments);
+
+
+export default router;
