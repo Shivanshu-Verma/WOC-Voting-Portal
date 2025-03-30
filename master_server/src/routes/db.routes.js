6536d41aa7c0f3488e6a0dbf0551344eb613e065
Aradhya2708 <aradhyamahajan2708@gmail.com>
2025-03-30 18:47:01 +0530

    add update routes and controllers

--- master_server/src/routes/db.routes.js
+++ master_server/src/routes/db.routes.js
@@ -1,16 +1,17 @@
 import express from "express";
 import {
-    getAllVoters,
-    getAllCandidates,
-    getAllVolunteers,
-    getAllStaff,
-    getAllEVM,
-    getAllCommitments
+    getAllVoters, updateVoter,
+    getAllCandidates, updateCandidate,
+    getAllVolunteers, updateVolunteer,
+    getAllStaff, updateStaff,
+    getAllEVM, updateEVM,
+    getAllCommitments, updateCommitment
 } from "../controllers/db.controller.js";
+import { authenticateUser, verifierIsStaff } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-// Route to get all voters
+// GET routes
 router.get("/voters", getAllVoters);
 router.get("/candidates", getAllCandidates);
 router.get("/volunteers", getAllVolunteers);
@@ -18,5 +19,12 @@ router.get("/staff", getAllStaff);
 router.get("/evm", getAllEVM);
 router.get("/commitments", getAllCommitments);
 
+// PUT (Edit) routes
+router.put("/voters/:id", authenticateUser, verifierIsStaff, updateVoter);
+router.put("/candidates/:id", authenticateUser, verifierIsStaff, updateCandidate);
+router.put("/volunteers/:id", authenticateUser, verifierIsStaff, updateVolunteer);
+router.put("/staff/:id", authenticateUser, verifierIsStaff, updateStaff);
+router.put("/evm/:id", authenticateUser, verifierIsStaff, updateEVM);
+router.put("/commitments/:id", authenticateUser, verifierIsStaff, updateCommitment);
 
 export default router;
